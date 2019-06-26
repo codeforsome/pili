9024754ad8998e2c5e6aef87a811d4f1a529b63c
man <857578569@qq.com>
2019-06-26 20:28:02 +0800

    0626

--- public/js/index.js
+++ public/js/index.js
@@ -1,92 +1,130 @@
-
-
+//轮播图
 var slideshow = new Vue({
     delimiters: ['${', '}'],
     el: '#slideshow',
     data: {
-        imgUrl: [ ],
-        isTure: false,
+        imgList: [
+            {
+                url: "/public/images/slide1.jpg",
+                leftContent: "《东离剑游纪》插画大赛11.1开启！",
+                rightContent: "10月1日 优酷独播！"
+            },
+            {
+                url: "/public/images/slide2.jpg",
+                leftContent: "《霹雳魔封》2018年末新档大戏 11.23强势上线",
+                rightContent: "霹雳视频会员活动开启！"
+            },
+            {
+                url: "/public/images/slide3.jpg",
+                leftContent: "霹雳英雄战纪之刀说异数  普通话版",
+                rightContent: "刀说异数-重启篇！"
+            }
+        ],
+        hide: false,
         currentIndex: 0,
-        distance: 0, //图片移动的距离
-        minDistance: 0,
-        maxDistance: 0,
+        distance: 0, //图片移动的距离 1代表一张图片的距离 负数代表整体图片向左移动
+        leftDistance: 0,// 代表整体图片向左移动的最大距离 在mounted 中初始化该值。
+        rightDistance: 0,//代表整体图片向右移动的最大距离
         transitionStyle: {},
-        scale: 100, //移到的距离增加的比例为1倍即100%
+        scroll: {},
     },
-    created: function () {
-        this.maxDistance = 100;
+    mounted: function () {
+        this.leftDistance = - (this.imgList.length); //设置左边最大的移动数
+        this.moveQuick(-1); //图片整体向左移动一张图片
         this.autoPlay();
+        window.onscroll = () => {  // 监听窗口滚动 当滚动值大于轮播图大小 停止轮播图滚动
+            if (window.pageYOffset) {
+                this.scroll = {
+                    x: window.pageYOffset,
+                    y: window.pageXOffset
+                }
+            } else {
+                this.scroll = {
+                    x: document.documentElement.scrollLeft + document.body.scrollLeft,
+                    y: document.documentElement.scrollTop + document.body.scrollTop
+                }
+            }
+            if (this.scroll.x > this.getClientHeight()) {
+                this.stopPlay();
+            }
+        }
     },
     methods: {
-        timer: function () { },
+        timer: function () { }, //自动滚动图片的计时器
         autoPlay: function () {
             var _this = this;
             this.timer = setInterval(function () {
-                _this.rightMoveAll();
+                _this.leftMoveAll();
             }, 5000);
         },
-        stopPlay:function(){
+        stopPlay: function () {//停止自动滚动图片
             clearInterval(this.timer);
         },
-        moveDistance: function (distance, style) {
-            this.distance += distance;
+        moveDistance: function () {//移动图片
             this.transitionStyle = {
-                transition: style,
-                transform: 'translateX(' + this.distance + '%)',
+                transition: 'transform 1s ease-in-out',
+                transform: 'translateX(' + this.distance * 100 + '%)',
             };
         },
-        rightMoveAll: function () {
+        moveQuick: function (distance) {
+            this.distance = distance;
+            this.transitionStyle = {
+                transition: 'none',
+                transform: 'translateX(' + this.distance * 100 + '%)',
+            };
+        },
+        rightMoveAll: function () { //整体图片向右移动
+            this.distance++;
+            setTimeout(() => { //点击后图片介绍隐藏
+                this.hide = true;
+            }, 100);
+            setTimeout(() => {  //当图片切换成功后  显示新的图片介绍
+                this.currentIndex--;
+                if (this.currentIndex < 0) {
+                    this.currentIndex = this.imgList.length - 1;
+                }
+                this.hide = false;
+            }, 700);
+            if (this.distance > this.rightDistance) {
+                setTimeout(() => {
+                    this.distance = this.leftDistance + 1;
+                    this.moveDistance();
+                }, 200);
+                this.moveQuick(this.leftDistance); //快速移动到最后一张图
+                return;
+            }
+            this.moveDistance();
+        },
+        leftMoveAll: function () { //整体图片向左移动
+            this.distance--;
             setTimeout(() => {
-                this.isTure = true;
+                this.hide = true;
             }, 300);
             setTimeout(() => {
                 this.currentIndex++;
-                if (this.currentIndex >= this.imgUrl.length) {
+                if (this.currentIndex >= this.imgList.length) {
                     this.currentIndex = 0;
                 }
-                this.isTure = false;
+                this.hide = false;
             }, 700);
-            if (this.distance <= this.minDistance) {
+            if (this.distance < this.leftDistance) {
                 setTimeout(() => {
-                    this.moveDistance(-this.scale, 'transform 1s ease-in-out');
+                    this.distance = -1;
+                    this.moveDistance();
                 }, 100);
-                this.distance = 0;
-                this.moveDistance(0, 'none');
+                this.moveQuick(this.rightDistance); //快速移动到最前一张图
                 return;
             }
-            this.moveDistance(-this.scale, 'transform 1s ease-in-out');
+            this.moveDistance();
         },
-        leftMoveAll: function () {
-            setTimeout(() => {
-                this.isTure = true;
-            }, 300);
-            setTimeout(() => {
-                this.currentIndex--;
-                if (this.currentIndex < 0) {
-                    this.currentIndex = this.imgUrl.length - 1;
-                }
-                this.isTure = false;
-            }, 700);
-            if (this.distance >= this.maxDistance) {
-                setTimeout(() => {
-                    this.moveDistance(this.scale, 'transform 1s ease-in-out');
-                }, 100);
-                this.distance = this.minDistance + this.scale;
-                this.moveDistance(0, 'none');
-                return;
-            }
-            this.moveDistance(this.scale, 'transform 1s ease-in-out');
-        },
-        rightMove: function () {
-            // 初始化有问题  在数据初始化后赋值
-             this.minDistance = -this.imgUrl.length * this.scale;
-            clearInterval(this.timer);
-            this.rightMoveAll();
-            this.autoPlay();
-        },
-        leftMove: function () {
+        nextMove: function () { //移动到下一张图片
             clearInterval(this.timer);
             this.leftMoveAll();
+            this.autoPlay();
+        },
+        preMove: function () { //移动到前一张图片
+            clearInterval(this.timer);
+            this.rightMoveAll();
             this.autoPlay();
         },
         getClientHeight: function () {
@@ -95,10 +133,10 @@ var slideshow = new Vue({
                 clientHeight = window.innerHeight;
             else if ((document.body) && (document.body.clientHeight))
                 clientHeight = document.body.clientHeight;
-            return clientHeight + 'px';
+            return clientHeight;
         },
     },
-}) 
+});
 
 var container = new Vue({
     delimiters: ['${', '}'],
@@ -112,42 +150,18 @@ var container = new Vue({
             { title: '霹雳图库', imgUrl: '../public/images/new5.png' },
         ],
         currentIndex: 0,
-        itemMaxWidth: 33.3,
-        itemMinWidth: 16.6,
-        screenWidth: 0,
-        screenHeight: 0,
-        containerWidth: 850,
-        itemBGImag: '../public/images/bg.png',
-    },
-
-    mounted() {
-        this.screenWidth = document.body.clientWidth;
-        this.screenHeight = document.body.clientHeight;
-        window.onresize = () => {
-            return (() => {
-                this.screenWidth = document.body.clientWidth;
-                this.screenHeight = document.body.clientHeight;
-            })();
-        };
+        itemBGImag: '/public/images/bg.png',
     },
     methods: {
         show: function (index) {
             this.currentIndex = index;
         },
-        getItemWidth: function (index) {
-            if (this.screenWidth <= this.containerWidth) {
-                if (index < 3) {
-                    return this.itemMaxWidth;
-                } else {
-                    return 50;
-                }
+        seleted: function (index) {
+            if (this.currentIndex == index) {
+                return 'seleted'
             }
-            if (this.currentIndex == index)
-                return this.itemMaxWidth;
-            else
-                return this.itemMinWidth;
         },
-        getBGImage: function (index) {
+        getBGImage: function (index) { //如果是当前图片 返回背景色图
             if (this.currentIndex == index)
                 return this.itemBGImag;
             else
@@ -179,79 +193,48 @@ var reco = new Vue({
             { imgUrl: '../public/images/text5.jpg', title: '霹雳魔封', content: ' 龙脑青阳子久违再出，挺身率领圣龙口一战来自炽炼界的异魔者六弒荒魔！乱世之秋，三教再出，佛剑分说与剑子仙迹、疏楼龙宿，三人即将再续三教顶峰之会。面对三教之战，莫召奴、谈无欲、净琉璃又要如何力挽狂澜？欲知一连串精彩结果，尽在霹雳魔封。' }
         ],
         currentIndex: 0,
-        preIndex: 0,
-        distance: 50,
-        maxZIndex: 0,
-        maxScale: 1,
-        scale: 0.1,
-        isBegin: true,
-        itemZIndex: [],
+        distance: 50, //图片下移的大小
+        scale: 0.1, //图片每次缩小的比例
+        itemList: [], //保存状态
     },
-    created: function () {
+    mounted: function () {
         this.preIndex = 0;
-        this.maxZIndex = this.currentIndex = this.textList.length - 1;
-        for (var i = 0; i < this.textList.length; i++) {
-            this.itemZIndex.push(i);
+        let length = this.textList.length;
+        for (var i = 0; i < length; i++) {
+            let imgInfo = {};
+            imgInfo.index = i;
+            imgInfo.scale = 1 - i * this.scale;
+            imgInfo.zIndex = length - i;
+            imgInfo.top = i * this.distance;
+            this.itemList.push(imgInfo);
         }
     },
     methods: {
         isShow: function (index) {
             return index == this.currentIndex ? 'block' : 'none';
         },
+        prePage: function () {
+            this.textList.unshift(this.textList.pop());
+        },
         nextPage: function () {
-            this.isBegin = false;
-            this.preIndex = this.currentIndex--;
-            this.calculateZIndex();
-            if (this.currentIndex < 0) {
-                this.preIndex = this.currentIndex = this.textList.length - 1;
-                return;
+            this.textList.push(this.textList.shift());
+        },
+        liStyle: function (index) { 
+            var top = this.itemList[index].top;
+            var zIndex = this.itemList[index].zIndex;
+            return {
+                'top': top + 'px',
+                'z-index': zIndex,
             }
         },
-        prePage:function(){
-            this.isBegin = false;
-            this.preIndex = this.currentIndex++;
-            this.calculateZIndex();
-            if (this.currentIndex >this.textList.length-1) {
-                this.preIndex = this.currentIndex = 0;
-                return;
-            }
+        imgStyle: function (index) {
+            var scale = this.itemList[index].scale;
+            return { 'transform': 'scale(' + scale + ')' }
         },
-        calculateZIndex: function () {
-            var length = this.itemZIndex.length - 1;
-            for (var j = 0; j <= length; j++) {
-                if (this.itemZIndex[j] == length) this.itemZIndex[j] = 0;
-                else this.itemZIndex[j] = ++(this.itemZIndex[j]);
-            }
-        },
-        getSacle: function (index) {
-            if (this.isBegin) {
-                return (this.maxScale - (this.maxZIndex - index) * this.scale);
-            } else {
-                return (this.maxScale - (this.maxZIndex - this.itemZIndex[index]) * this.scale);
-            }
-        },
-        getZIndex: function (index) {
-            if (this.isBegin) {
-                return index;
-            } else {
-                return this.itemZIndex[index];
-            }
 
-        },
-        getTop: function (index) {
-            if (this.isBegin) {
-                return (this.maxZIndex - index) * this.distance;
-            } else {
-                return (this.maxZIndex - this.itemZIndex[index]) * this.distance;
-            }
 
-        }
     },
-    computed: {
-        styleObject: function () {
-            return "1111";
-        }
-    }
+
 })
 
 

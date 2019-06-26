8e414e3d565c3e3753d25dede20441f3201f1e91
man <857578569@qq.com>
2019-06-26 20:32:28 +0800

    0626

--- routers/routers.js
+++ routers/routers.js
@@ -1,5 +1,5 @@
 const user=require('./user');
-const data=require('./data');
+const view=require('./view');
 const admin=require('./admin');
 const figure=require('./figure');
 const music=require('./music');
@@ -8,7 +8,7 @@ const index=require('./index');
 exports.use=function(app){
     app.use('/',index);
     app.use('/user',user); //处理用户的请求
-    app.use('/data',data)  //返回前端所需要的各个页面
+    app.use('/view',view)  //返回前端所需要的各个页面
     app.use('/admin',admin);// 管理员
     app.use('/figure',figure);// 霹雳人物
     // app.use('/music',music);//霹雳音乐
